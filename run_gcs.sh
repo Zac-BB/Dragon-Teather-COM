593c35106e746efa754035157a2341ccc56d3376
Zac <zac.serocki@gmail.com>
2026-05-04 20:34:40 -0400

    stuff

--- run_gcs.sh
+++ run_gcs.sh
@@ -2,6 +2,6 @@ sudo ip addr flush dev enx00e04c3600c7
 sudo ip addr add 192.168.208.1/24 dev enx00e04c3600c7
 sudo ip link set enx00e04c3600c7 up
 
-
+echo "$(ip addr)"
 # source Documents/dragon/Dragon-Teather-COM/venv/bin/activate
 # python Documents/dragon/Dragon-Teather-COM/dragon_pi_sender.py
